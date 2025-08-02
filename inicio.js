9b576a778c4846fad3684711d0dfca10603518ab
jhormanquina17-arch <jhormanquina17@gmail.com>
2025-08-02 11:47:20 -0500

    subiendo archivos

---
+++ inicio.js
@@ -0,0 +1,10 @@
+let numero=12;
+console.log("el valor del numero:"+numero);
+let nombre="jhorman";
+let edad=18;
+let sueldo=200000
+let hijos=2
+console.log(" hola " + nombre  +"\n tienes " + edad + " años de edad "+"\nnumero de hijos deseados es:"+ hijos +"\n sueldo deseado " + sueldo);
+let femenino=false;
+let masculino=true;
+console.lon("mi genero es","femenino",femenino,"maculino",masculino )
