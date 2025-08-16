991e13a7f293e8d45cdf693723cf73cc10a08375
jhormanquina17-arch <jhormanquina17@gmail.com>
2025-08-15 21:32:39 -0500

    cuadrado.js

---
+++ cuadrado.js
@@ -0,0 +1,5 @@
+let num1=prompt("ingrese el valor numero 1");
+let num2=prompt("ingrese el valor del lado 2");
+let multiplicacion= num1 * num2;
+
+alert("el area de un cuadrado es :" + multiplicacion )
---
+++ operadoresConparacion.js
@@ -0,0 +1,16 @@
+let num=(8>2);
+console.log("el 8>2 es:" + num);
+let num1=(2<1);
+console.log("el 2<1 es:" + num1);
+let num2=(2==1);
+console.log ("el 2==1 es:" + num2);
+let num3=(2=="2");
+console.log("el 2=='2' es :" + num3);
+let num4=(2==='2');
+console.log("el 2==='2' es:" + num4);
+//par o impar
+if(7%2==0);
+{console.log("impar");
+
+}
+7%2==0 ? console.log("par"):console.log("impar");
